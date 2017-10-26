cded8d2220d584e95dd80ddd67934ab9e80e908d
Diego Medina <fmpwizard@gmail.com>
2017-10-25 22:41:31 -0400

    custom:Applied patch go-mgo/mgo#295

--- session.go
+++ session.go
@@ -3148,19 +3148,23 @@ func prepareFindOp(socket *mongoSocket, op *queryOp, limit int32) bool {
 		panic("invalid query collection name: " + op.collection)
 	}
 
+	// Diego: applied patch from https://github.com/go-mgo/mgo/pull/295
 	find := findCmd{
-		Collection:  op.collection[nameDot+1:],
-		Filter:      op.query,
-		Projection:  op.selector,
-		Sort:        op.options.OrderBy,
-		Skip:        op.skip,
-		Limit:       limit,
-		MaxTimeMS:   op.options.MaxTimeMS,
-		MaxScan:     op.options.MaxScan,
-		Hint:        op.options.Hint,
-		Comment:     op.options.Comment,
-		Snapshot:    op.options.Snapshot,
-		OplogReplay: op.flags&flagLogReplay != 0,
+		Collection:      op.collection[nameDot+1:],
+		Filter:          op.query,
+		Projection:      op.selector,
+		Sort:            op.options.OrderBy,
+		Skip:            op.skip,
+		Limit:           limit,
+		MaxTimeMS:       op.options.MaxTimeMS,
+		MaxScan:         op.options.MaxScan,
+		Hint:            op.options.Hint,
+		Comment:         op.options.Comment,
+		Snapshot:        op.options.Snapshot,
+		Tailable:        op.flags&flagTailable != 0,
+		AwaitData:       op.flags&flagAwaitData != 0,
+		OplogReplay:     op.flags&flagLogReplay != 0,
+		NoCursorTimeout: op.flags&flagNoCursorTimeout != 0,
 	}
 	if op.limit < 0 {
 		find.BatchSize = -op.limit
